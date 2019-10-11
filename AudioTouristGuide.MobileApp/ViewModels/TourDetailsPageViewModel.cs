5a6ad0865a3cca577156ae55e78404faaf727a00
Aleksei Starkov <alexey.starkov@protonmail.com>
2019-10-11 22:58:06 +0200

    ATG-13

---
+++ AudioTouristGuide.MobileApp/ViewModels/TourDetailsPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using AudioTouristGuide.MobileApp.Models;
+using AudioTouristGuide.MobileApp.ViewModels.BaseObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AudioTouristGuide.MobileApp.ViewModels
+{
+    public class TourDetailsPageViewModel : ViewModelBase
+    {
+        private ATGTourDetailedModel _tour;
+        public ATGTourDetailedModel Tour
+        {
+            get { return _tour; }
+            set { SetProperty(ref _tour, value); }
+        }
+
+        public TourDetailsPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+        }
+
+        public override void OnNavigatingTo(INavigationParameters parameters)
+        {
+            base.OnNavigatingTo(parameters);
+
+
+        }
+    }
+}
